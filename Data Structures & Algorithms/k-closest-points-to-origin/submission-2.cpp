4cbbd41df53e5c2e9ffec826308d1c80a4619665
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-06-02 20:31:54 +0530

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+class Solution {
+    priority_queue<vector<double>> heap; //max heap
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        for (const auto& point : points) {
+            double dist = sqrt(point[0] * point[0] + point[1] * point[1]);
+
+            if (heap.size() == k) {
+                vector<double> farthest = heap.top();
+                if (dist < farthest[0]) {
+                    heap.pop();
+                    vector<double> temp = {dist, double(point[0]), double(point[1])};
+                    heap.push(temp);
+                }
+            }
+            else {
+                vector<double> temp = {dist, double(point[0]), double(point[1])};
+                heap.push(temp);
+            }
+        }
+
+        vector<vector<int>> kclosest;
+        while (!heap.empty()) {
+            vector<double> temp = heap.top();
+            heap.pop();
+            kclosest.push_back({int(temp[1]), int(temp[2])});
+        }
+
+        return kclosest;
+    }
+};
