331367a9001086f227787700b091d1d11b7f5698
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-06-11 23:53:54 +0530

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    void dfs(vector<vector<int>>& image, int r, int c, int color, int startcolor) {
+        int rows = image.size(), cols = image[0].size();
+        if(r<0 || c<0 || r==rows || c==cols || image[r][c]!=startcolor || image[r][c]==color) {
+            return;
+        }
+
+        image[r][c] = color;
+        dfs(image, r+1, c, color, startcolor);
+        dfs(image, r-1, c, color, startcolor);
+        dfs(image, r, c+1, color, startcolor);
+        dfs(image, r, c-1, color, startcolor);
+    }
+
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        dfs(image, sr, sc, color, image[sr][sc]);
+        return image;
+    }
+};
