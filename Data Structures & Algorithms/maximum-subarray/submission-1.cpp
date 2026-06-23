842fd687d41c4d03bb7917e0d2cd36bd6a8e501e
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-06-23 23:21:39 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,17 @@
+// Kadane's Algorithm:
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0], currSum = 0;
+        for (int num : nums) {
+            currSum = max(currSum, 0);
+            currSum += num;
+            maxSum = max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+};
