53c1f36349ba193f5eab3d2125460967053a5409
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-06-29 20:51:51 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int L = 0, R = numbers.size()-1;
+
+        while(L < R) {
+            if (numbers[L] + numbers[R] > target) {
+                R--;
+            }
+            else if (numbers[L] + numbers[R] < target) {
+                L++;
+            }
+            else {
+                return vector<int>{L+1, R+1}; // 1-indexed, not zero-indexed
+            }
+        }
+
+        return vector<int>{};
+    }
+};
