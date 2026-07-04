b7de8f66cc177a18aaf55f56fe8ac30ee72ab5ca
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-07-04 23:34:43 +0530

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+/* We can't use 2-pointers nor a sliding window, since the numbers can be 
+positive, negative, or 0: -1,000 <= nums[i] <= 1,000. So, we can't guarantee
+that the sum will keep increasing nor decreasing.
+Ex: 2 = 2, 2 + -1 + 1 = 2, -1 + 1 + 2 = 2, and 2 = 2 in nums = [2,-1,1,2], k = 2
+Here, the sum can become 2 again later on if the sum of the other numbers 
+in the contiguous subarray cancel each other out/become 0. (2 + -1 + 1 = 2)
+*/
+
+/* In hash map: unordered_map<prefixSum, count>
+prefixSum is the sum that we can remove from the front/prefix of the 
+subarray to make it equal to k.
+count is the the number of times that prefixSum has occurred
+*/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int currSum = 0, result = 0;
+        unordered_map<int, int> prefixSums;
+        prefixSums[0]++;
+
+        for(int n: nums) {
+            currSum += n;
+            result += prefixSums[currSum-k];
+            prefixSums[currSum]++;
+        }
+
+        return result;
+    }
+};
