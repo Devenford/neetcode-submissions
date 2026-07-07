7315e2151630c1e087e551c0f9a6a9810abf30ac
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-07-07 19:30:47 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,48 @@
+#include<unordered_map>
+using namespace std;
+
+struct TrieNode {
+    unordered_map<char, TrieNode*> children;
+    bool word = false;
+};
+
+class PrefixTree {
+    TrieNode *root;
+public:
+    PrefixTree() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode *curr = root;
+        for (char c : word) {
+            if (curr->children.count(c)==0) {
+                curr->children[c] = new TrieNode();
+            }
+            curr = curr->children[c];
+        }
+        curr->word = true;
+    }
+    
+    bool search(string word) {
+        TrieNode *curr = root;
+        for (char c: word) {
+            if (curr->children.count(c) == 0) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return curr->word;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode *curr = root;
+        for (char c: prefix) {
+            if (curr->children.count(c) == 0) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return true;
+    }
+};
