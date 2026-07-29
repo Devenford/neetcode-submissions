e8b8eed66d6df6250acd8ad0ea7d628c92710022
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-07-29 20:30:10 +0530

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.cpp
@@ -0,0 +1,43 @@
+#include<unordered_set>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        // undirected, weighted graph
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+        minHeap.push({0, 0, 0}); // {wt, i, j}
+        vector<vector<int>> deltas = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        int t = 0, rows = grid.size(), cols = grid[0].size();
+
+        while(!minHeap.empty()) {
+            vector<int> cell = minHeap.top();
+            minHeap.pop();
+            t = max(cell[0], t);
+            int i = cell[1], j = cell[2];
+            if (grid[i][j] == -1) { // already visited through the lowest cost path
+                continue;
+            }
+
+            if (i == rows-1 && j == cols-1) {
+                break;
+            }
+
+            for (int k=0; k<4; k++) {
+                int r = i + deltas[k][0], c = j + deltas[k][1];
+                if (r>=0 && r<rows && c>=0 && c<cols && grid[r][c] != -1) {
+                    // != -1 check is for an already visited cell
+                    minHeap.push({max(grid[i][j], grid[r][c]), r, c});
+                }
+            }
+
+            grid[i][j] = -1;  // -1 = visited
+        }
+
+        return t;
+    }
+};
