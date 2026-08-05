859455fa4c2863e84944e5dea9148a2c4b5dc4fb
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-08-06 02:20:34 +0530

    Add: zeroOneKnapsack - submission-6

---
+++ Data Structures & Algorithms/zeroOneKnapsack/submission-6.cpp
@@ -0,0 +1,32 @@
+// bottom-up dp approach
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maximumProfit(vector<int>& profit, vector<int>& weight, int capacity) {
+        int N = weight.size(), M = capacity;
+        vector<vector<int>> dp(N, vector<int> (M+1, 0));
+
+        //initialize first row
+        for (int c = 0; c <= M; c++) { // c = curr capacity
+            if (weight[0] <= c) {
+                dp[0][c] = profit[0];
+            }
+        }
+
+        for(int i = 1; i<N ; i++) {
+            for(int c = 1; c <= M; c++) {
+                int maxProfit = dp[i-1][c];  // skipping it
+
+                if (weight[i] <= c) {
+                    maxProfit = max(maxProfit, profit[i] + dp[i-1][c-weight[i]]); // including it
+                }
+                dp[i][c] = maxProfit;
+            }
+        }
+
+        return dp[N-1][M];
+    }
+};
