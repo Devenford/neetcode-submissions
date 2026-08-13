77c8b2401f429220215e67f8a80684f369b81ac0
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-08-13 19:08:52 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.cpp
@@ -0,0 +1,33 @@
+// dp (bottom-up)
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> prev(amount+1, 0);
+        
+        for(int c=0; c<=amount; c++) {
+            if (c % coins[0] == 0) {
+                prev[c] = 1;
+            }
+        }
+
+        for(int i=1; i<coins.size(); i++) {
+            vector<int> dp(amount+1, 0);
+            for(int c=0; c<=amount; c++) {
+                dp[c] = prev[c]; // exclude it
+
+                int remainingAmount = c - coins[i];
+                if (remainingAmount >= 0) {
+                    dp[c] = dp[c] + dp[remainingAmount];
+                }
+            }
+
+            prev = dp;
+        }
+
+        return prev[amount];
+    }
+};
