91588e0026eaa27fba46b1f42cf7a345503ddc70
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-09-05 16:30:47 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,27 @@
+// Using a monotonically decreasing stack
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size());
+        stack<int> s; // stores index positions
+        s.push(0);
+        for(int i=1; i<temperatures.size(); i++) {
+            while(!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                result[s.top()] = i - s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+
+        while(!s.empty()) {
+            result[s.top()] = 0;
+            s.pop();
+        }
+
+        return result;
+    }
+};
