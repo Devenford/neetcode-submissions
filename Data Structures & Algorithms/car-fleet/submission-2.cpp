289f9606fcf7de8f8d4e51239d1f2ade4d3b4dbf
Khaushl Dev Saravanan <156348611+Devenford@users.noreply.github.com>
2026-09-06 13:42:08 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,28 @@
+// using a stack]
+#include<stack>
+#include<vector>
+#include<utility>
+using namespace std;
+
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> posSpeed; // position-speed
+        for(int i=0; i<position.size(); i++) {
+            posSpeed.push_back({position[i], speed[i]});
+        }
+        sort(posSpeed.rbegin(), posSpeed.rend()); // sort in descending order
+
+        stack<double> s;
+        // Stack top = arrival time of the latest fleet formed so far, i.e. the slowest car so far
+        for(pair<int,int> &p : posSpeed) {
+            double remTime = ((double) target - p.first) / p.second;
+
+            if (s.empty() || remTime > s.top()) {
+                s.push(remTime);
+            }
+        }
+
+        return s.size();
+    }
+};
